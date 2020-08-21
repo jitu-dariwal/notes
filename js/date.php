e9c72f07b4ea1fd78a4ebb4df46d56af6731783e
Prashant Sharma <prashant@dotsquares.com>
2020-08-21 10:08:37 +0530

    update the details

--- js/date.php
+++ js/date.php
@@ -8,4 +8,11 @@
 		console.log("Today var : "+today);
 		console.log("Today : "+ new Date());
     }, 1000);
+	
+	var d = new Date("February 29, 2012");
+	console.log("Date ", d.toLocaleDateString());
+	d.setMonth(d.getMonth() + 1);
+	console.log("Next Month Date", d.toLocaleDateString());
+	d.setYear(d.getFullYear() + 1);
+	console.log("Next Year Date", d.toLocaleDateString());
 </script>
